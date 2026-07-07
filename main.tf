89ca7e01fba323d99d2008f89fead7c3a498d99a
Paulina-cmd <paulinavelasquez2008@gmail.com>
2026-07-07 12:17:03 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,8 +15,6 @@ resource "github_repository" "mi_repo_desde_terraform" {
   name        = "repo-desde-terraform"
   description = "Prueba xd"
   visibility  = "public"
-
-  # Configuraciones "pro" que le dan más utilidad al repo
   has_issues = true
   has_wiki   = false
   auto_init  = true
